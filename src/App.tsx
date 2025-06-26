9df00e3251f595bc1265b1b8ad106a3a0b84cc5e
aivanns <a1vazovski@yandex.ru>
2025-06-26 22:04:40 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -33,8 +33,8 @@ function App() {
     position: "the best frontend developer in the world",
     description: "made this shit.",
     socialLinks: {
-      twitter: "хз не спросил ещё",
-      discord: "codeineware"
+      twitter: "не",
+      discord: "aivazovski"
     }
   }
 ]
